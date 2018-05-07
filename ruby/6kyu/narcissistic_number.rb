368cc5b758cc3091299db778d3dcdc89ed9ec560
Clara Ameller <claraameller7@gmail.com>
2018-05-07 19:54:30 +0200

    Narcissistic number

---
+++ ruby/6kyu/narcissistic_number.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# A Narcissistic Number is a number which is the sum of its own digits,
+# each raised to the power of the number of digits.
+
+# Your code must return true or false depending upon whether the given number is
+# a Narcissistic number.
+# Error checking for text strings or other invalid inputs is not required,
+# only valid integers will be passed into the function.
+
+# 153 (3 digits): 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+# 1634 (4 digits): 1^4 + 6^4 + 3^4 + 4^4 = 1 + 1296 + 81 + 256 = 1634
+
+def narcissistic?(value)
+  sum = 0
+  value.to_s.split('').each do |v|
+    sum += v.to_i**value.to_s.length
+  end
+  value == sum
+end
+
+narcissistic?(153)
